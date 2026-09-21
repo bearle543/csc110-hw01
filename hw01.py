e5fac4d528eb16f7967e78bcfa6baa84d0366921
Brioni Earle <bearle@smith.edu>
2026-09-21 11:03:07 -0400

    completed steps 1 to 4

--- hw01.py
+++ hw01.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
-#       Peers: (add any collaborators)
+#        Name: Brioni Earle
+#       Peers: None
 #  References: (anything you checked to solve this)
 # ------------------------------------------------------
 
@@ -46,12 +46,40 @@ def main():
     # =============================================
     # Your code for part 1 under this line and before the print statements
 
+    x=(27)
+    y=(1)
+    a=(1.5)
+    b=(7)
+    c=(-1)
+    result1=(3*x-9*y)/(2*a*(b-c))
+    
+    print(result1)
+    
+    print("Part 1: x =", x)
+    print("Part 1: y =", y)
+    print("Part 1: a =", a)
+    print("Part 1: b =", b)
+    print("Part 1: c =", c)
+    print("Part 1: result =", result1)
+    
+
+
+
     # End of Part 1 ----------------------
 
 
     # Part 2: Power
     # =============================================
     # Your code for part 2 under this line and before the print statements
+
+
+    x=(5)
+    y=(-3)
+    result2=(x**2)*(y**4)
+    print("Part 2: x = 5")
+    print("Part 2: y = -3")
+    print("Part 2: result =", result2)
+
 
     # End of Part 2 ----------------------
 
@@ -61,12 +89,26 @@ def main():
     # =============================================
     # Your code for part 3 under this line and before the print statements
 
+
+    a=(100)
+    b=(13)
+    print("Part 3: a =", a)
+    print("Part 3: b =", b)
+    print("Part 3: result =", a//b)
+
+
+
     # End of Part 3 ----------------------
 
 
     # Part 4: Modulo
     # =============================================
     # Your code for part 4 under this line and before the print statements
+    
+    
+    result4=(a%b)
+    print("Part 4: result =", result4)
+    
 
     # End of Part 4 ----------------------
 
